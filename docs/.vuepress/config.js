c42cfc8898d842c248ecc53933744ce44e6602d7
GJ Wang <itellboy@foxmail.com>
2020-04-02 11:19:09 +0800

    docs: edit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,7 @@ module.exports = {
         collapsable: false,
         sidebarDepth: 1,
         children: [
-          ['/monthly/m202003', '03月']
+          ['/monthly/m202003', '03月'],
           ['/monthly/m202004', '04月']
         ]
       }
